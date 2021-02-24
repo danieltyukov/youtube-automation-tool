378be94e1f70c7a722826ea513709f4e41aa5804
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-02-24 16:48:46 +0200

    further development of url handling

--- youtube-automation.py
+++ youtube-automation.py
@@ -9,3 +9,13 @@ def look_for_new_video():
     base_video_url = "https://www.youtube.com/watch?v="
     base_search_url = "https://www.googleapis.com/youtube/v3/search?"
     
+    url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=1'.format(api_key, channel_id)
+    inp = urllib.urloppen(url)
+    resp = json.load(inp)
+    
+    vidID = resp['items'][0]['id']['videoID']
+    
+    video_exists = False
+    with open('videoid.json', 'r') as json_file:
+        data = json.load(json_file)
+        
