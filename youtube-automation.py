e0e51eaf43c3e921b71a7d496f53a0c69a475a3d
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-02-24 17:07:46 +0200

    final push

--- youtube-automation.py
+++ youtube-automation.py
@@ -10,7 +10,7 @@ def look_for_new_video():
     base_search_url = "https://www.googleapis.com/youtube/v3/search?"
     
     url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=1'.format(api_key, channel_id)
-    inp = urllib.urloppen(url)
+    inp = urllib.urlopen(url)
     resp = json.load(inp)
     
     vidID = resp['items'][0]['id']['videoID']
@@ -18,4 +18,19 @@ def look_for_new_video():
     video_exists = False
     with open('videoid.json', 'r') as json_file:
         data = json.load(json_file)
-        
+        if data['videoId'] != vidID:
+            driver = webdriver.Firefox()
+            driver.get(base_video_url + vidID)
+            video_exists = True
+            
+    if video_exists:
+        with open('videoid.json', 'w') as json_file:
+            data = {'videoId' : vidID}
+            json.dump(data, json_file)
+            
+try:
+    while True:
+        look_for_new_video()
+        time.sleep(10)
+except KeyboardInterrupt:
+    print('stopping')
